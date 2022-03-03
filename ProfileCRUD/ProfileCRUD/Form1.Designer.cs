fdfd6ce85664bc0a78268c5f9cb5ed0b42737dd2
Mitchell Humphries <50331840+MLHumphries@users.noreply.github.com>
2022-03-03 15:55:13 -0600

    Add files via upload

---
+++ ProfileCRUD/ProfileCRUD/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace ProfileCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_reg = new System.Windows.Forms.TextBox();
+            this.txt_FirstName = new System.Windows.Forms.TextBox();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_DisplayAll = new System.Windows.Forms.Button();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.dataGrid_Display = new System.Windows.Forms.DataGridView();
+            this.regIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.profileDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.authenticte1DataSet = new ProfileCRUD.Authenticte1DataSet();
+            this.profileDetailsTableAdapter = new ProfileCRUD.Authenticte1DataSetTableAdapters.ProfileDetailsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid_Display)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authenticte1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reg ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // txt_reg
+            // 
+            this.txt_reg.Location = new System.Drawing.Point(210, 45);
+            this.txt_reg.Name = "txt_reg";
+            this.txt_reg.Size = new System.Drawing.Size(457, 22);
+            this.txt_reg.TabIndex = 4;
+            // 
+            // txt_FirstName
+            // 
+            this.txt_FirstName.Location = new System.Drawing.Point(210, 83);
+            this.txt_FirstName.Name = "txt_FirstName";
+            this.txt_FirstName.Size = new System.Drawing.Size(457, 22);
+            this.txt_FirstName.TabIndex = 5;
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Location = new System.Drawing.Point(210, 120);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(457, 22);
+            this.txt_LastName.TabIndex = 6;
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(227, 253);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(102, 43);
+            this.btn_Insert.TabIndex = 8;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Enabled = false;
+            this.btn_Update.Location = new System.Drawing.Point(345, 253);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(102, 43);
+            this.btn_Update.TabIndex = 9;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Enabled = false;
+            this.btn_Delete.Location = new System.Drawing.Point(467, 253);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(102, 43);
+            this.btn_Delete.TabIndex = 10;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(587, 253);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(102, 43);
+            this.btn_Cancel.TabIndex = 11;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(721, 349);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(102, 43);
+            this.btn_Search.TabIndex = 12;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // btn_DisplayAll
+            // 
+            this.btn_DisplayAll.Location = new System.Drawing.Point(865, 530);
+            this.btn_DisplayAll.Name = "btn_DisplayAll";
+            this.btn_DisplayAll.Size = new System.Drawing.Size(137, 54);
+            this.btn_DisplayAll.TabIndex = 13;
+            this.btn_DisplayAll.Text = "Display All Records";
+            this.btn_DisplayAll.UseVisualStyleBackColor = true;
+            this.btn_DisplayAll.Click += new System.EventHandler(this.btn_DisplayAll_Click);
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Location = new System.Drawing.Point(210, 359);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(457, 22);
+            this.txt_Search.TabIndex = 14;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(210, 155);
+            this.txt_Address.Multiline = true;
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(457, 92);
+            this.txt_Address.TabIndex = 15;
+            // 
+            // dataGrid_Display
+            // 
+            this.dataGrid_Display.AutoGenerateColumns = false;
+            this.dataGrid_Display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid_Display.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.regIDDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dataGrid_Display.DataSource = this.profileDetailsBindingSource;
+            this.dataGrid_Display.Location = new System.Drawing.Point(78, 397);
+            this.dataGrid_Display.Name = "dataGrid_Display";
+            this.dataGrid_Display.RowHeadersWidth = 51;
+            this.dataGrid_Display.RowTemplate.Height = 24;
+            this.dataGrid_Display.Size = new System.Drawing.Size(781, 187);
+            this.dataGrid_Display.TabIndex = 16;
+            this.dataGrid_Display.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_Display_CellContentClick);
+            // 
+            // regIDDataGridViewTextBoxColumn
+            // 
+            this.regIDDataGridViewTextBoxColumn.DataPropertyName = "RegID";
+            this.regIDDataGridViewTextBoxColumn.HeaderText = "RegID";
+            this.regIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.regIDDataGridViewTextBoxColumn.Name = "regIDDataGridViewTextBoxColumn";
+            this.regIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            this.firstNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.firstNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            this.lastNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // profileDetailsBindingSource
+            // 
+            this.profileDetailsBindingSource.DataMember = "ProfileDetails";
+            this.profileDetailsBindingSource.DataSource = this.authenticte1DataSet;
+            // 
+            // authenticte1DataSet
+            // 
+            this.authenticte1DataSet.DataSetName = "Authenticte1DataSet";
+            this.authenticte1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // profileDetailsTableAdapter
+            // 
+            this.profileDetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 637);
+            this.Controls.Add(this.dataGrid_Display);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.btn_DisplayAll);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.txt_LastName);
+            this.Controls.Add(this.txt_FirstName);
+            this.Controls.Add(this.txt_reg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid_Display)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profileDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.authenticte1DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_reg;
+        private System.Windows.Forms.TextBox txt_FirstName;
+        private System.Windows.Forms.TextBox txt_LastName;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_DisplayAll;
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.DataGridView dataGrid_Display;
+        private Authenticte1DataSet authenticte1DataSet;
+        private System.Windows.Forms.BindingSource profileDetailsBindingSource;
+        private Authenticte1DataSetTableAdapters.ProfileDetailsTableAdapter profileDetailsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn regIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ ProfileCRUD/ProfileCRUD/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProfileCRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Authenticte1;Integrated Security=True;");
+
+        public void DisplayData()
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from [ProfileDetails]";
+            cmd.ExecuteNonQuery();
+            DataTable dta = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dta);
+            dataGrid_Display.DataSource = dta;
+            conn.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'authenticte1DataSet.ProfileDetails' table. You can move, or remove it, as needed.
+            //this.profileDetailsTableAdapter.Fill(this.authenticte1DataSet.ProfileDetails);
+
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            SqlCommand sqcmd = new SqlCommand("Select * from [ProfileDetails] where RegID='" + txt_reg.Text + "'", conn);
+
+            SqlDataAdapter da = new SqlDataAdapter(sqcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if(dt.Rows.Count > 0)
+            {
+                conn.Close();
+                MessageBox.Show("Reg ID already exists");
+            }
+            else
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Insert into [ProfileDetails] (RegID, FirstName, LastName, Address) values ('" + txt_reg.Text + "','" + txt_FirstName.Text + "','" + txt_LastName.Text + "','" + txt_Address.Text + " ')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                txt_reg.Text = "";
+                txt_FirstName.Text = "";
+                txt_LastName.Text = "";
+                txt_Address.Text = "";
+
+                DisplayData();
+                MessageBox.Show("Data successfully inserted.");
+            }
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update [ProfileDetails] set FirstName='" + txt_FirstName.Text + "', LastName='" + txt_LastName.Text + "', Address='" + txt_Address.Text + "' where RegID='" + txt_reg.Text + "'";
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            DisplayData();
+
+            txt_reg.Text = "";
+            txt_FirstName.Text = "";
+            txt_LastName.Text = "";
+            txt_Address.Text = "";
+
+            btn_Insert.Enabled = true;
+            btn_Update.Enabled = false;
+            btn_Delete.Enabled = false;
+
+            MessageBox.Show("Data updated");
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from [ProfileDetails] where RegID='" + txt_reg.Text + "'";
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            DisplayData();
+
+            txt_reg.Text = "";
+            txt_FirstName.Text = "";
+            txt_LastName.Text = "";
+            txt_Address.Text = "";
+
+            btn_Insert.Enabled = true;
+            btn_Update.Enabled = false;
+            btn_Delete.Enabled = false;
+
+            txt_reg.Enabled = true;
+
+            MessageBox.Show("Data deleted.");
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string search = txt_Search.Text;
+            SqlCommand sqcmd = new SqlCommand("Select * from [ProfileDetails] where FirstName Like '%" + search + "%' OR LastName Like '%" + search + "%' OR Address Like '%" + search + "%'", conn);
+
+            SqlDataAdapter da = new SqlDataAdapter(sqcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if(dt.Rows.Count > 0)
+            {
+                dataGrid_Display.DataSource = dt;
+                conn.Close();
+            }
+            else
+            {
+                MessageBox.Show("No record found.");
+                conn.Close();
+            }
+        }
+
+        private void btn_DisplayAll_Click(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            conn.Close();
+            DisplayData();
+
+            txt_reg.Text = "";
+            txt_FirstName.Text = "";
+            txt_LastName.Text = "";
+            txt_Address.Text = "";
+
+            btn_Insert.Enabled = true;
+            btn_Update.Enabled = false;
+            btn_Delete.Enabled = false;
+
+            txt_reg.Enabled = true;
+        }
+
+        private void dataGrid_Display_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+            DataGridViewRow row = dataGrid_Display.Rows[i];
+
+            string reg = row.Cells[0].Value.ToString();
+            string value1 = row.Cells[1].Value.ToString();
+            string value2 = row.Cells[2].Value.ToString();
+            string value3 = row.Cells[3].Value.ToString();
+
+            txt_reg.Text = reg;
+            txt_reg.Enabled = false;
+
+            txt_FirstName.Text = value1;
+            txt_LastName.Text = value2;
+            txt_Address.Text = value3;
+
+            btn_Insert.Enabled = false;
+            btn_Update.Enabled = true;
+            btn_Delete.Enabled = true;
+        }
+    }
+}
